d07ecfa24cc1fa6d04daec4c79be89d3a1708e70
reydaniel <reynaldidaniel@outlook.com>
2021-03-27 16:33:57 +0700

    Create interface about magic 8 ball. It will randomly pick an image every tapping the button

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var theAnswer: UIImageView!
+    
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
 
-
-
+    @IBAction func askButton(_ sender: UIButton) {
+        theAnswer.image = ballArray.randomElement()
+    }
+    
 }
 
